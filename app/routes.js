e88e59c72e51223115c04d20953525704ad6547f
Braeden Sowinski <sowinskibraeden@gmail.com>
2022-01-05 15:51:50 -0800

    update/site-revamp

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-
-module.exports = function (app, server) {
-    app.get('/', function (req, res) {
-        res.render('index');
-    });
-}
